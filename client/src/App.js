39be785c58e0f8963fe1a67e1805a9cf75dbae93
Knoxj1 <knoxjarvis@gmail.com>
2019-08-20 18:16:49 -0400

    90 percent done with part 2, some errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
-
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -13,6 +14,9 @@ const App = () => {
     <div>
       <SavedList list={savedList} />
       <div>Replace this Div with your Routes</div>
+          <Route exact path="/" component={MovieList}/>
+          <Route path="/movie/:id" /> 
+
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const MovieList = props => {
   const [movies, setMovies] = useState([])
@@ -30,7 +31,7 @@ const MovieList = props => {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
+    <Link to = "/movies/${movie:id} " className="movie-card">
       <h2>{title}</h2>
       <div className="movie-director">
         Director: <em>{director}</em>
@@ -45,7 +46,7 @@ function MovieDetails({ movie }) {
           {star}
         </div>
       ))}
-    </div>
+    </Link>
   );
 }
 
